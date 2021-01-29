e79cca598f63374e164e1471812c083de0ec14c0
csae8092 <peter.andorfer@oeaw.ac.at>
2021-01-29 10:18:33 +0100

    generic netvis added

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,15 +2,13 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'Django Base Project',
+    'title': 'MMP',
     'author': 'Peter Andorfer',
-    'subtitle': 'A django project to bootstrap further web-app developments',
-    'description': 'This django projects includes some generic applications\
-    (webpage, vocabs, entities, bib)',
-    'github': 'https://github.com/acdh-oeaw/djangobaseproject',
-    'purpose_de': 'der Schaffung einer einheitlichen\
-    und generischen Grundlage für Web Applikationen.',
-    'purpose_en': 'to bootstrap web development.',
+    'subtitle': 'Mapping Medieval Peoples: Visualizing Semantic Landscapes in Early Medieval Europe',
+    'description': 'During the last decades, historical research on the early Middle Ages has rejected the notion that ethnic identities were determined by common blood and origin, and they were shown to be socially constructed – which does not mean that they were entirely malleable. Case studies on specific ethnic entities have proven the validity of this assessment. The ERC AdG project SCIRE at the Institute for Medieval Research (2011-16) has yielded new approaches to medieval identities. In that context, ethnicity can first of all be regarded as a mental map of the social world: a cognitive tool to understand large social groupings and their agency in the political landscape. Studies of ethnonyms and of ethnic terminology have already clarified that picture, and it is time to take the next step: Mapping Medieval Peoples (MMP) proposes an innovative technical solution to analyze and visualize this rich material. It attempts to reconstruct ‘mental maps’ of a world of peoples, and will offer insights into the interplay between ethnonyms, religious terms, as well as attributes, traits and stereotypes, thus surveying the language employed to create ethnic entities and to distinguish them from each other. The resulting web application featuring a rich set of explorative functions (searching, browsing, and interactive visualization) along the main dimensions, will provide an overview of identity construction and also allow for detailed studies on particular terms and discursive patterns.',
+    'github': 'https://github.com/acdh-oeaw/mmp',
+    'purpose_de': 'Veröffentlichung von Forschungsdaten.',
+    'purpose_en': 'Publication of research data.',
     'version': '0.0.1',
     'matomo_id': 'provide some',
     'matomo_url': '//matomo.acdh.oeaw.ac.at/',
