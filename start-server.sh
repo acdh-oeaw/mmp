b0650bff9e362079ba97d992bcf3ab1d74a2a1a8
csae8092 <peter.andorfer@oeaw.ac.at>
2021-02-01 09:10:28 +0100

    migrate back to start-server

--- start-server.sh
+++ start-server.sh
@@ -5,4 +5,5 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; 
     (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python djangobaseproject/manage.py createsuperuser --no-input --noinput --email 'blank@email.com' --settings=djangobaseproject.settings.docker)
 fi
 cd djangobaseproject && python manage.py collectstatic --no-input --settings=djangobaseproject.settings.docker &&
+python manage.py migrate --no-input --settings=djangobaseproject.settings.docker &&
 gunicorn djangobaseproject.wsgi_docker --user www-data --bind 0.0.0.0:8010 --workers 3 & nginx -g "daemon off;"
