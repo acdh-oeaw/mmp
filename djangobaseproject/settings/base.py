f2842be9aaa576f2bcbb5158c023e8a21f8ac34d
csae8092 <peter.andorfer@oeaw.ac.at>
2021-05-31 13:31:13 +0200

    minor

--- djangobaseproject/settings/base.py
+++ djangobaseproject/settings/base.py
@@ -55,10 +55,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
-    'DEFAULT_FILTER_BACKENDS':
-        [
-            'django_filters.rest_framework.DjangoFilterBackend'
-        ]
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
 }
 
 MIDDLEWARE = [
@@ -154,7 +153,7 @@ VOCABS_SETTINGS = {
 
 SERIALIZATION_MODULES = {
     "geojson": "django.contrib.gis.serializers.geojson",
- }
+}
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (40, 10),
