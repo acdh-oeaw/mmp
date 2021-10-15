96b404af61b9788a94a1a55b396a53389a9d7be9
csae8092 <peter.andorfer@oeaw.ac.at>
2021-10-15 12:05:03 +0200

    function to process texts added #64

---
+++ archiv/text_processing.py
@@ -0,0 +1,23 @@
+import cltk
+from cltk import NLP
+cltk_nlp_lat = NLP(language="lat")
+to_remove = [
+    cltk.lexicon.processes.LatinLexiconProcess,
+    cltk.embeddings.processes.LatinEmbeddingsProcess,
+]
+for x in to_remove:
+    cltk_nlp_lat.pipeline.processes.remove(x)
+
+
+def string_to_dict(my_text, lang_model=cltk_nlp_lat):
+    cltk_doc = lang_model.analyze(text=my_text)
+    processed_text = [
+        {
+            'token': x.string,
+            'pos': x.pos,
+            'lemma': x.lemma,
+            'named_entity': x.named_entity,
+            'index_sentence': x.index_sentence
+        } for x in cltk_doc.words
+    ]
+    return processed_text
