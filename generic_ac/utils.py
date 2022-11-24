b8e4345fd183cc6b6308ce088bbfe4070130077e
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-24 09:18:14 +0100

    minor: formatting

--- generic_ac/utils.py
+++ generic_ac/utils.py
@@ -4,20 +4,15 @@ from django.db.models import Q
 
 def query(q, config):
     counter = 0
-    response = {
-        "q": q,
-        "next": None,
-        "previous": None,
-        "results": []
-    }
+    response = {"q": q, "next": None, "previous": None, "results": []}
     for x in config:
         or_condition = Q()
-        app_name = x['app_name'].lower()
-        model_name = x['model_name'].lower()
+        app_name = x["app_name"].lower()
+        model_name = x["model_name"].lower()
         model = ContentType.objects.get(
             app_label=app_name, model=model_name
         ).model_class()
-        for search_field in x['search_fields']:
+        for search_field in x["search_fields"]:
             or_condition.add(Q(**{f"{search_field}__icontains": q}), Q.OR)
         for obj in model.objects.filter(or_condition).distinct():
             counter += 1
@@ -28,9 +23,9 @@ def query(q, config):
                 "label": obj.__str__(),
             }
             try:
-                item['url'] = obj.get_absolute_url()
+                item["url"] = obj.get_absolute_url()
             except AttributeError:
-                item['url'] = None
-            response['results'].append(item)
-    response['count'] = counter
+                item["url"] = None
+            response["results"].append(item)
+    response["count"] = counter
     return response
