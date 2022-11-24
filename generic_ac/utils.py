b66044f5be65c15f3c8bf916ecc0286e6a2eee81
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-24 09:50:25 +0100

    wip: limit and page are working * still an issue with `next` param

--- generic_ac/utils.py
+++ generic_ac/utils.py
@@ -1,10 +1,11 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Q
 
 
-def query(q, config):
-    counter = 0
-    response = {"q": q, "next": None, "previous": None, "results": []}
+def query(q, config, limit=5, page=1):
+    total_count = 0
+    response = {"q": q, "next": None, "previous": None, "count": None, "results": []}
     for x in config:
         or_condition = Q()
         app_name = x["app_name"].lower()
@@ -14,18 +15,33 @@ def query(q, config):
         ).model_class()
         for search_field in x["search_fields"]:
             or_condition.add(Q(**{f"{search_field}__icontains": q}), Q.OR)
-        for obj in model.objects.filter(or_condition).distinct():
-            counter += 1
-            item = {
-                "id": obj.id,
-                "kind": model_name,
-                "app_name": app_name,
-                "label": obj.__str__(),
-            }
-            try:
-                item["url"] = obj.get_absolute_url()
-            except AttributeError:
-                item["url"] = None
-            response["results"].append(item)
-    response["count"] = counter
+        qs = model.objects.filter(or_condition).distinct()
+        total_count += qs.count()
+        p = Paginator(model.objects.filter(or_condition).distinct(), limit)
+        next_page = False
+        try:
+            p.page(page)
+            has_objects = True
+        except EmptyPage:
+            has_objects = False
+        if has_objects:
+            if p.page(page).has_next:
+                next_page = True
+            for obj in p.page(page).object_list:
+                item = {
+                    "id": obj.id,
+                    "kind": model_name,
+                    "app_name": app_name,
+                    "label": obj.__str__(),
+                }
+                try:
+                    item["url"] = obj.get_absolute_url()
+                except AttributeError:
+                    item["url"] = None
+                response["results"].append(item)
+    response["count"] = total_count
+    if next_page:
+        response["next"] = page + 1
+    if page > 1:
+        response["previous"] = page - 1
     return response
