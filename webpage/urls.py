c19a9d0e8c88aec35ce2cdef83a34ee7520c4515
csae8092 <p.andorfer@gmail.com>
2026-09-23 07:10:39 +0200

    chore: removes no longer needed webpage views/code

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,25 +1,21 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from . import views
 from django_spaghetti.views import Plate
 
+from . import views
 
-app_name = 'webpage'
+app_name = "webpage"
 
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
 
 urlpatterns = [
-    path('imprint', views.ImprintView.as_view(), name="imprint"),
+    path("imprint", views.ImprintView.as_view(), name="imprint"),
     path(
-        'data-model',
-        Plate.as_view(
-            plate_template_name="webpage/data_model.html"
-        ),
-        name="data_model"
+        "data-model",
+        Plate.as_view(plate_template_name="webpage/data_model.html"),
+        name="data_model",
     ),
-    path('', views.GenericWebpageView.as_view(), name="start"),
-    path('accounts/login/', views.user_login, name='user_login'),
-    path('logout/', views.user_logout, name='user_logout'),
-    path('project-info/', views.project_info, name='project_info'),
-    path('<slug:template>', views.GenericWebpageView.as_view(), name='staticpage'),
+    path("", views.GenericWebpageView.as_view(), name="start"),
+    path("project-info/", views.project_info, name="project_info"),
+    path("<slug:template>", views.GenericWebpageView.as_view(), name="staticpage"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -2,13 +2,10 @@ from copy import deepcopy
 
 import requests
 from django.conf import settings
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.template import loader
 from django.views.generic import TemplateView
 
-from .forms import form_user_login
 from .metadata import PROJECT_METADATA as PM
 
 
@@ -48,42 +45,6 @@ class GenericWebpageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["apps"] = settings.INSTALLED_APPS
         return context
-
-    def get_template_names(self):
-        template_name = "webpage/{}.html".format(self.kwargs.get("template", "index"))
-        try:
-            loader.select_template([template_name])
-            template_name = "webpage/{}.html".format(
-                self.kwargs.get("template", "index")
-            )
-        except:
-            template_name = "webpage/index.html"
-        return [template_name]
-
-
-#################################################################
-#               views for login/logout                          #
-#################################################################
-
-
-def user_login(request):
-    if request.method == "POST":
-        form = form_user_login(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd["username"], password=cd["password"])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(request.GET.get("next", "/"))
-            return HttpResponse("user does not exist")
-    else:
-        form = form_user_login()
-        return render(request, "webpage/user_login.html", {"form": form})
-
-
-def user_logout(request):
-    logout(request)
-    return render(request, "webpage/user_logout.html")
 
 
 def handler404(request, exception):
